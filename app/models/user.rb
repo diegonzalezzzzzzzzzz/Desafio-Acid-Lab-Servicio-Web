6dd4a30a85fb1dad8b2554f3ebe9b9699d41938d
diegonzalezzzzzzzzzz <kenderson04@gmail.com>
2018-02-21 23:20:05 -0300

    Subo la estructura del proyecto authentication_project al repositorio de GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: true, 
                     :email_format => {:message => 'is not looking good'}
+                    
 
 end
 
